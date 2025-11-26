06222a2490ac655101bf78add1d9daef4849269b
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-11-26 20:45:36 +0300

    fix(stat-dto): добавить аннотации валидации в EndpointHitDto

--- stat/stat-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,6 +1,8 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,12 +15,16 @@ public class EndpointHitDto {
 
     private Long id;
 
+    @NotBlank
     private String app;
 
+    @NotBlank
     private String uri;
 
+    @NotBlank
     private String ip;
 
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
