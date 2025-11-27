299921769ec3c03ede53ddd530be191b0879dbd8
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-11-27 17:54:40 +0300

    feat(main-service): драфт сущностей для главного сервиса

---
+++ main-service/src/main/java/ru/practicum/ewm/model/compilation/Compilation.java
@@ -0,0 +1,31 @@
+package ru.practicum.ewm.model.compilation;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.ewm.model.event.Event;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "compilations")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Compilation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TODO: ManyToMany
+    private Set<Event> events;
+
+    @Column(name = "pinned")
+    private Boolean pinned;
+
+    @Column(name = "title")
+    private String title;
+}
---
+++ main-service/src/main/java/ru/practicum/ewm/model/event/Event.java
@@ -0,0 +1,68 @@
+package ru.practicum.ewm.model.event;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.dto.event.Location;
+import ru.practicum.ewm.model.category.Category;
+import ru.practicum.ewm.model.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "annotation")
+    private String annotation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @ToString.Exclude
+    private Category category;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn = LocalDateTime.now();
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "event_date")
+    private LocalDateTime eventDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "initiator")
+    @ToString.Exclude
+    private User initiator;
+
+    // тут пока хз, нужно что-то типа @Embedded, т.к. это связанная сущность, не имеющая смысла без Event
+    private Location location;
+
+    @Column(name = "paid")
+    private Boolean paid;
+
+    @Column(name = "participant_limit")
+    private Integer participantLimit;
+
+    @Column(name = "published_on")
+    private LocalDateTime publishedOn;
+
+    @Column(name = "request_moderation")
+    private Boolean requestModeration;
+
+    @Column(name = "state")
+    @Enumerated(EnumType.STRING)
+    private EventState state = EventState.PENDING; // подумать насчёт PENDING
+
+    @Column(name = "title")
+    private String title;
+}
