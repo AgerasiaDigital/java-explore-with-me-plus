57fd3a016fe994544f411a330162aeb7dfb8fae6
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-11-27 21:36:11 +0300

    feat(main-service): добавить интерфейс UserService и имплементацию

---
+++ main-service/src/main/java/ru/practicum/ewm/service/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package ru.practicum.ewm.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ru.practicum.ewm.dto.user.NewUserRequest;
+import ru.practicum.ewm.dto.user.UserDto;
+import ru.practicum.ewm.dto.user.UserParam;
+import ru.practicum.ewm.exception.EmailConflictException;
+import ru.practicum.ewm.mapper.UserMapper;
+import ru.practicum.ewm.model.user.User;
+import ru.practicum.ewm.repository.UserRepository;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UserDto> get(UserParam userParam) {
+        log.debug("Get user request: {}", userParam);
+
+        if (userParam.hasIds()) {
+            // TODO: мб может понадобиться сортировка
+            return userRepository.findByIdIn(userParam.ids()).stream()
+                    .map(UserMapper::toUserDto)
+                    .toList();
+        } else {
+            // TODO: custom pageable impl?
+            int page = userParam.from() / userParam.size();
+            Pageable pageable = PageRequest.of(page, userParam.size());
+
+            return userRepository.findAll(pageable).get()
+                    .map(UserMapper::toUserDto)
+                    .toList();
+        }
+    }
+
+    @Override
+    public UserDto create(NewUserRequest request) {
+        log.debug("New user request: {}", request);
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            log.warn("User with email = {} already exists", request.getEmail());
+            throw new EmailConflictException(String.format("User with email = %s already exists", request.getEmail()));
+        }
+
+        User user = userRepository.save(UserMapper.toNewUser(request));
+
+        log.info("New user added: {}", user);
+        return UserMapper.toUserDto(user);
+    }
+
+    @Override
+    public void deleteById(Long userId) {
+        log.debug("User delete request with id = {}", userId);
+        userRepository.deleteById(userId);
+        log.info("User with id = {} has been deleted", userId);
+    }
+}
