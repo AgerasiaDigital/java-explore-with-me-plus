76b61f5fcea0ca0e9e2005254d4c862a19706cf4
Denis Nagovitsyn <disochkin@gmail.com>
2025-12-03 08:39:38 +0500

    добавил ендпоинт для редактирование админом ролевой доступ

---
+++ main-service/src/main/java/ru/practicum/ewm/dto/user/Role.java
@@ -0,0 +1,6 @@
+package ru.practicum.ewm.dto.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- main-service/src/main/java/ru/practicum/ewm/event/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/event/EventRepository.java
@@ -1,11 +1,12 @@
 package ru.practicum.ewm.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.ewm.model.event.Event;
 
 import java.util.Collection;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long>, QuerydslPredicateExecutor<Event> {
 
     Collection<Event> findAllByInitiatorId(Long id);
 
